77cc4919d23b11176eba79e06badb427b4eb8c2d
Rafael Marinho de Araújo <rma6@cin.ufpe.br>
2019-09-20 17:10:50 -0300

    added function for ds3231

--- i2c.h
+++ i2c.h
@@ -5,6 +5,18 @@
 #include <drivers/i2c/adi_i2c.h>
 #endif
 
+#define CLOCK_ADDRESS 0x68
+
+/*#ifdef ADI_I2C_CFG_MCTL_STRETCHSCL
+#undef ADI_I2C_CFG_MCTL_STRETCHSCL
+#define ADI_I2C_CFG_MCTL_STRETCHSCL (1)
+#endif
+
+#ifdef ADI_I2C_CFG_ASTRETCH_MST
+#undef ADI_I2C_CFG_ASTRETCH_MST
+#define ADI_I2C_CFG_ASTRETCH_MST (0b0010)
+#endif*/
+
 extern uint8_t i2c_mem[ADI_I2C_MEMORY_SIZE];
 extern ADI_I2C_HANDLE i2c_device;
 extern uint32_t i2c_hwErrors;
@@ -24,4 +36,7 @@ ADI_I2C_RESULT i2cRead(uint16_t const SlaveAddress, uint8_t bytes, uint8_t* data
 //bytes: number of bytes to be written
 //data: pointer to data that's going to be written
 ADI_I2C_RESULT i2cWrite(uint16_t const SlaveAddress, uint8_t bytes, uint8_t* data);
+
+ADI_I2C_RESULT i2cWriteDS3231(uint8_t bytes, uint8_t* data);
+ADI_I2C_RESULT i2cReadDS3231(uint8_t bytes, uint8_t* data);
 #endif /* I2C_H_ */
